bdf3bc17bad17997bddd7d4aee0a60768580327c
Victorkajgard <victorkajgard@gmail.com>
2024-03-19 16:02:29 +0100

    19/3

--- client/src/components/ProductItemSmall.jsx
+++ client/src/components/ProductItemSmall.jsx
@@ -14,7 +14,7 @@ import {
 } from "@mui/material";
 import { addToCart } from "../services/CartService";
 
-function avgRating(product) {
+export function avgRating(product) {
 let avgRating = 0;
 
   product.ratings.length > 0 &&
@@ -25,6 +25,7 @@ let avgRating = 0;
 
     
     return avgRating/product.ratings.length
+    
 
 }
 
