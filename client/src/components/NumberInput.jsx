29467c7c6089168386b8fe577ad4fe895c72a7ce
ludwigstrand <ludwig.strand@live.com>
2024-03-19 22:59:17 +0100

    Funkar att klicka -+ i productlarge och lägga i kundkorg

--- client/src/components/NumberInput.jsx
+++ client/src/components/NumberInput.jsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import { Unstable_NumberInput as BaseNumberInput } from '@mui/base/Unstable_NumberInput';
-import { styled } from '@mui/system';
-import RemoveIcon from '@mui/icons-material/Remove';
-import AddIcon from '@mui/icons-material/Add';
+import * as React from "react";
+import { Unstable_NumberInput as BaseNumberInput } from "@mui/base/Unstable_NumberInput";
+import { styled } from "@mui/system";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
 
 const NumberInput = React.forwardRef(function CustomNumberInput(props, ref) {
   return (
@@ -16,7 +16,7 @@ const NumberInput = React.forwardRef(function CustomNumberInput(props, ref) {
       slotProps={{
         incrementButton: {
           children: <AddIcon fontSize="small" />,
-          className: 'increment',
+          className: "increment",
         },
         decrementButton: {
           children: <RemoveIcon fontSize="small" />,
@@ -28,57 +28,65 @@ const NumberInput = React.forwardRef(function CustomNumberInput(props, ref) {
   );
 });
 
-export default function QuantityInput() {
-  return <NumberInput aria-label="Quantity Input" min={1} max={99} />;
+export default function QuantityInput({ onChange, value }) {
+  return (
+    <NumberInput
+      aria-label="Quantity Input"
+      min={1}
+      max={99}
+      value={value}
+      onChange={(_, quantity) => onChange(quantity)}
+    />
+  );
 }
 
 const blue = {
-  100: '#daecff',
-  200: '#b6daff',
-  300: '#66b2ff',
-  400: '#3399ff',
-  500: '#007fff',
-  600: '#0072e5',
-  700: '#0059B2',
-  800: '#004c99',
+  100: "#daecff",
+  200: "#b6daff",
+  300: "#66b2ff",
+  400: "#3399ff",
+  500: "#007fff",
+  600: "#0072e5",
+  700: "#0059B2",
+  800: "#004c99",
 };
 
 const grey = {
-  50: '#F3F6F9',
-  100: '#E5EAF2',
-  200: '#DAE2ED',
-  300: '#C7D0DD',
-  400: '#B0B8C4',
-  500: '#9DA8B7',
-  600: '#6B7A90',
-  700: '#434D5B',
-  800: '#303740',
-  900: '#1C2025',
+  50: "#F3F6F9",
+  100: "#E5EAF2",
+  200: "#DAE2ED",
+  300: "#C7D0DD",
+  400: "#B0B8C4",
+  500: "#9DA8B7",
+  600: "#6B7A90",
+  700: "#434D5B",
+  800: "#303740",
+  900: "#1C2025",
 };
 
-const StyledInputRoot = styled('div')(
+const StyledInputRoot = styled("div")(
   ({ theme }) => `
   font-family: 'IBM Plex Sans', sans-serif;
   font-weight: 400;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[500]};
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[500]};
   display: flex;
   flex-flow: row nowrap;
   justify-content: center;
   align-items: center;
-`,
+`
 );
 
-const StyledInput = styled('input')(
+const StyledInput = styled("input")(
   ({ theme }) => `
   font-size: 0.875rem;
   font-family: inherit;
   font-weight: 400;
   line-height: 1.375;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-  border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
+  color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+  background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+  border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
   box-shadow: 0px 2px 4px ${
-    theme.palette.mode === 'dark' ? 'rgba(0,0,0, 0.5)' : 'rgba(0,0,0, 0.05)'
+    theme.palette.mode === "dark" ? "rgba(0,0,0, 0.5)" : "rgba(0,0,0, 0.05)"
   };
   border-radius: 8px;
   margin: 0 8px;
@@ -94,16 +102,18 @@ const StyledInput = styled('input')(
 
   &:focus {
     border-color: ${blue[400]};
-    box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[700] : blue[200]};
+    box-shadow: 0 0 0 3px ${
+      theme.palette.mode === "dark" ? blue[700] : blue[200]
+    };
   }
 
   &:focus-visible {
     outline: 0;
   }
-`,
+`
 );
 
-const StyledButton = styled('button')(
+const StyledButton = styled("button")(
   ({ theme }) => `
   font-family: 'IBM Plex Sans', sans-serif;
   font-size: 0.875rem;
@@ -111,9 +121,9 @@ const StyledButton = styled('button')(
   line-height: 1.5;
   border: 1px solid;
   border-radius: 999px;
-  border-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[200]};
-  background: ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
-  color: ${theme.palette.mode === 'dark' ? grey[200] : grey[900]};
+  border-color: ${theme.palette.mode === "dark" ? grey[800] : grey[200]};
+  background: ${theme.palette.mode === "dark" ? grey[900] : grey[50]};
+  color: ${theme.palette.mode === "dark" ? grey[200] : grey[900]};
   width: 32px;
   height: 32px;
   display: flex;
@@ -126,8 +136,8 @@ const StyledButton = styled('button')(
 
   &:hover {
     cursor: pointer;
-    background: ${theme.palette.mode === 'dark' ? blue[700] : blue[500]};
-    border-color: ${theme.palette.mode === 'dark' ? blue[500] : blue[400]};
+    background: ${theme.palette.mode === "dark" ? blue[700] : blue[500]};
+    border-color: ${theme.palette.mode === "dark" ? blue[500] : blue[400]};
     color: ${grey[50]};
   }
 
@@ -138,5 +148,5 @@ const StyledButton = styled('button')(
   &.increment {
     order: 1;
   }
-`,
+`
 );
--- client/src/components/ProductItemLarge.jsx
+++ client/src/components/ProductItemLarge.jsx
@@ -15,99 +15,90 @@ import {
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { addToCart } from "../services/CartService";
-import { Unstable_NumberInput as BaseNumberInput } from "@mui/base/Unstable_NumberInput";
-import QuantityInput from "./NumberInput";
 import NumberInput from "./NumberInput";
 import { avgRating } from "./ProductItemSmall";
 
 //Lagt till onRatingAdd som in, rad 18
 function ProductItemLarge({ product, onRatingAdd }) {
   const [value, setValue] = useState(0);
-
-  let amount = 0;
+  const [quantity, setQuantity] = useState(1);
 
   return (
-      <Card sx={{ width: 500, mb: 10 }}>
-        <CardActionArea>
-          <CardMedia
-            // sx={{ height: 100 }}
-            component="img"
-            image={product.imageUrl}
-            alt="fruit"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {product.title}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <Typography component="legend" sx={{ ml: 2 }}>
-          Rating:
-        </Typography>
-        <Box>
-          <Rating
-            sx={{ ml: 2 }}
-            name="simple-controlled"
-            defaultValue={avgRating(product)}
-            onChange={(event, rating) => {
-              setValue(rating);
-            }}
-            // Lagt till rad 50
-            onSave={onRatingAdd}
-            precision={.5}
-          />
-          <Button
-            //Ändrat rad 55
-            onClick={() => onRatingAdd(value)}
-            //  onClick={() => addRating(product.id, value)}
-            variant="contained"
-            sx={{ ml: 4 }}
-          >
-            Skicka rating
-          </Button>
-        </Box>
-
-        <CardActions sx={{ ml: 1 }}>
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            sx={{ fontSize: 20, mr: 2 }}
-          >
-            {product.price} kr
-          </Typography>
-          <NumberInput
-            sx={{ mr: 10 }}
-            slotProps={{
-              input: {
-                // this exactly the same as onInputChange above
-                onChange: (event) =>
-                  console.log(`the input value is: ${event.target.value}`),
-              },
-            }}
-          />
-          <Button
-            sx={{ mr: 10 }}
-            variant="contained"
-            size="small"
-            color="primary"
-            onClick={() => addToCart(1, product.id, 1)}
-          >
-            Köp
-          </Button>
-        </CardActions>
+    <Card sx={{ width: 500, mb: 10 }}>
+      <CardActionArea>
+        <CardMedia
+          // sx={{ height: 100 }}
+          component="img"
+          image={product.imageUrl}
+          alt="fruit"
+        />
         <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            {product.description}
+          <Typography gutterBottom variant="h5" component="div">
+            {product.title}
           </Typography>
         </CardContent>
-        <CardActions>
-          <Button size="small" color="info" variant="contained">
-            <Link to={`/products/${product.id}/edit`}>
-            Ändra
-            </Link>
-          </Button>
-        </CardActions>
-      </Card>
+      </CardActionArea>
+      <Typography component="legend" sx={{ ml: 2 }}>
+        Rating:
+      </Typography>
+      <Box>
+        <Rating
+          sx={{ ml: 2 }}
+          name="simple-controlled"
+          defaultValue={avgRating(product)}
+          onChange={(event, rating) => {
+            setValue(rating);
+          }}
+          // Lagt till rad 50
+          onSave={onRatingAdd}
+          precision={0.5}
+        />
+        <Button
+          //Ändrat rad 55
+          onClick={() => onRatingAdd(value)}
+          //  onClick={() => addRating(product.id, value)}
+          variant="contained"
+          sx={{ ml: 4 }}
+        >
+          Skicka rating
+        </Button>
+      </Box>
+
+      <CardActions sx={{ ml: 1 }}>
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          sx={{ fontSize: 20, mr: 2 }}
+        >
+          {product.price} kr
+        </Typography>
+
+        <NumberInput
+          onChange={(value) => setQuantity(value)}
+          sx={{ mr: 5 }}
+        />
+        
+        <Button
+          sx={{ mr: 10 }}
+          variant="contained"
+          size="small"
+          color="primary"
+          onClick={() => addToCart(quantity, product.id, 1)}
+        >
+          Köp
+        </Button>
+      </CardActions>
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {product.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" color="admin" variant="contained">
+          <Link to={`/products/${product.id}/edit`}>Ändra</Link>
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
 
